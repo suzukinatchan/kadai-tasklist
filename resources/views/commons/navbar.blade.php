b03e1ce848baca59b412d76c0736834cb6cc4359
suzukinatchan <mj.smile.n@gmail.com>
2018-09-11 11:27:56 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,3 +1,7 @@
+//ログイン認証がされている人のナビバーは、Users,MyProfile,logoutの三つで
+//ログイン認証がされていない人はsignup,loginの二つになる。
+//tasklistナビバーはどっちでもついてる。
+
 <header>
     <nav class="navbar navbar-inverse navbar-static-top">
         <div class="container">
@@ -12,13 +16,24 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li>{!! link_to_route('tasks.create', '新規taskの投稿') !!}</li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
-                    <li><a href="#">Login</a></li>
+                    @if (Auth::check())
+                        <li><a href="#">Users</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">My profile</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
+                            </ul>
+                        </li>
+                        
+                    @else
+                        <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
+                        <li>{!! link_to_route('login', 'Login') !!}</li>
+                    @endif
                 </ul>
             </div>
-        </div>
     </nav>
 </header>
+                    
+                   
