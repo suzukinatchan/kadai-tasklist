d3b1b8116487bd0c9df6f69ac2e5628153c56318
suzukinatchan <mj.smile.n@gmail.com>
2018-09-13 07:41:12 +0000

    pgsql作動しないことによるmigrationやり直し

--- database/migrations/2018_09_13_125232_create_tasks_table.php
+++ database/migrations/2018_09_13_125232_create_tasks_table.php
@@ -16,12 +16,13 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned()->index();
-            $table->string('status');
+            $table->string('status,');
             $table->string('content');
             $table->timestamps();
             
-            // 外部キー制約
+             // 外部キー制約
             $table->foreign('user_id')->references('id')->on('users');
+            
         });
     }
 
